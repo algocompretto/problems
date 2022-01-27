95ae5bbfce9d3509c4fc593d3a3c44b454c93238
Gustavo Scholze <scholzegustavo@gmail.com>
2022-01-26 21:09:44 -0300

    update

--- search/maze.py
+++ search/maze.py
@@ -58,8 +58,31 @@ class Maze:
         if ml.column + 1 < self._columns and self._grid[ml.row][ml.column + 1] != Cell.BLOCKED:
             locations.append(MazeLocation(ml.row, ml.column+1))
         if ml.column - 1 >= 0 and self._grid[ml.row][ml.column-1] != Cell.BLOCKED:
-            locations.append(MazeLocations(ml.row, ml.column-1))
+            locations.append(MazeLocation(ml.row, ml.column-1))
         return locations
 
-maze: Maze = Maze()
-print(maze)
+    def mark(self, path: List[MazeLocation]):
+        for maze_location in path:
+            self._grid[maze_location.row][maze_location.column] = Cell.PATH
+        self._grid[self.start.row][self.start.column] = Cell.START
+        self._grid[self.goal.row][self.goal.column] = Cell.GOAL
+
+    def clear(self, path: List[MazeLocation]):
+        for maze_location in path:
+            self._grid[maze_location.row][maze_location.column] = Cell.EMPTY
+        self._grid[self.start.row][self.start.column] = Cell.START
+        self._grid[self.goal.row][self.goal.column] = Cell.GOAL
+
+if __name__ == "__main__":
+    m: Maze = Maze()
+    print(f"Labirinto: \n{m}")
+
+    solution1: Optional[Node[MazeLocation]] = dfs(m.start, m.goal_test, m.successors)
+
+    if solution1 is None:
+        print("No solution found using depth-first search!")
+    else:
+        path1: List[MazeLocation] = node_to_path(solution1)
+        m.mark(path1)
+        print(m)
+        m.clear(path1)
